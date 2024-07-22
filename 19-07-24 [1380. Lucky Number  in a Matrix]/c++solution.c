7b6b434e00b75fe56a84ddd87431dc8e8c0defd6
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-07-22 17:29:30 +0530

    Update c++solution.c

--- 19-07-24 [1380. Lucky Number  in a Matrix]/c++solution.c
+++ 19-07-24 [1380. Lucky Number  in a Matrix]/c++solution.c
@@ -1,14 +1,3 @@
-/*    "copy & past only solution function for getting result"  */
-
-#include <iostream>
-#include <vector>
-#include <climits>
-#include <algorithm>
-
-using namespace std;
-
-//----------------------------------------------------------------------------------------------------
-
 class Solution {
 public:
     vector<int> luckyNumbers(const vector<vector<int>>& matrix) {
@@ -41,25 +30,3 @@ public:
         return luckyNums;
     }
 };
-
-//----------------------------------------------------------------------------------------------------
-
-int main() {
-    Solution sol;
-
-    vector<vector<int>> matrix = {
-        {3, 7, 8},
-        {9, 11, 13},
-        {15, 16, 17}
-    };
-
-    vector<int> luckyNumbers = sol.luckyNumbers(matrix);
-
-    cout << "Lucky numbers: ";
-    for (int num : luckyNumbers) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
