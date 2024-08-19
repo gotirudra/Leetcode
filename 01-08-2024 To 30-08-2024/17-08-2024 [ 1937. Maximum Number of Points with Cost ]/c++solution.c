20c31b5fdcee4ce88cec84143f91630d8dab2192
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-08-19 22:35:29 +0530

    Create c++solution.c

---
+++ 01-08-2024 To 30-08-2024/17-08-2024 [ 1937. Maximum Number of Points with Cost ]/c++solution.c
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    long long maxPoints(vector<vector<int>>& grid) {
+        const int width = grid[0].size();
+        vector<long long> current(width), previous(width);
+        long long maxScore = 0;
+
+        for (const auto& level : grid) {
+            long long peak = 0;
+            
+            for (int i = 0; i < width; ++i) {
+                peak = max(peak - 1, previous[i]);
+                current[i] = peak;
+            }
+            peak = 0;
+           
+            for (int i = width - 1; i >= 0; --i) {
+                peak = max(peak - 1, previous[i]);
+                current[i] = max(current[i], peak) + level[i];
+            }
+            previous.swap(current);
+        }
+
+       
+        maxScore = *max_element(previous.begin(), previous.end());
+        return maxScore;
+    }
+};
+
+static const auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
