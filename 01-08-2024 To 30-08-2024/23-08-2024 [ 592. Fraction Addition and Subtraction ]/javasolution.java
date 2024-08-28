756aaeb02755430a2e17bcb3d84e153234d9d2f6
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-08-28 19:52:34 +0530

    Update javasolution.java

--- 01-08-2024 To 30-08-2024/23-08-2024 [ 592. Fraction Addition and Subtraction ]/javasolution.java
+++ 01-08-2024 To 30-08-2024/23-08-2024 [ 592. Fraction Addition and Subtraction ]/javasolution.java
@@ -1,31 +1,52 @@
-class Solution {
+public class Solution {
+
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    private int lcm(int a, int b) {
+        return (a / gcd(a, b)) * b;
+    }
+
+    private void reduce(int[] fraction) {
+        int gcd_ab = gcd(Math.abs(fraction[0]), fraction[1]);
+        fraction[0] /= gcd_ab;
+        fraction[1] /= gcd_ab;
+    }
+
+    private int parseNumber(String expression, int[] i) {
+        int sign = 1;
+        if (expression.charAt(i[0]) == '-') {
+            sign = -1;
+            i[0]++;
+        } else if (expression.charAt(i[0]) == '+') {
+            i[0]++;
+        }
+        int num = 0;
+        while (i[0] < expression.length() && Character.isDigit(expression.charAt(i[0]))) {
+            num = num * 10 + (expression.charAt(i[0]) - '0');
+            i[0]++;
+        }
+        return sign * num;
+    }
+
     public String fractionAddition(String expression) {
         int numerator = 0, denominator = 1;
-        
-        Pattern pattern = Pattern.compile("([+-]?\\d+)/(\\d+)");
-        Matcher matcher = pattern.matcher(expression);
-        
-        while (matcher.find()) {
-            int num = Integer.parseInt(matcher.group(1));
-            int den = Integer.parseInt(matcher.group(2));
-            
-            numerator = numerator * den + num * denominator;
-            denominator *= den;
-            
-            int gcdVal = gcd(Math.abs(numerator), denominator);
-            numerator /= gcdVal;
-            denominator /= gcdVal;
+        int[] i = {0}; // Use array to mimic passing by reference
+        while (i[0] < expression.length()) {
+            int num = parseNumber(expression, i);
+            i[0]++; // Skip the '/'
+            int den = parseNumber(expression, i);
+            int LCM = lcm(denominator, den);
+            numerator = numerator * (LCM / denominator) + num * (LCM / den);
+            denominator = LCM;
         }
-        
-        return numerator + "/" + denominator;
+        if (numerator == 0) return "0/1";
+        int[] fraction = {numerator, denominator};
+        reduce(fraction);
+        return fraction[0] + "/" + fraction[1];
     }
+
     
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
 }
