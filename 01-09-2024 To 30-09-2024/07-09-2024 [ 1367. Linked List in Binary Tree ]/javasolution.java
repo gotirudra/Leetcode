b2379ae0386dfa75358ae4f8252a9d61aa20d32b
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-09-07 22:31:18 +0530

    Create javasolution.java

---
+++ 01-09-2024 To 30-09-2024/07-09-2024 [ 1367. Linked List in Binary Tree ]/javasolution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        return dfs(head, head, root);
+    }
+    
+    private boolean dfs(ListNode head, ListNode cur, TreeNode root) {
+        if (cur == null) return true; 
+        if (root == null) return false; 
+        
+        if (cur.val == root.val) {
+            cur = cur.next;  
+        } else if (head.val == root.val) {
+            head = head.next; 
+        } else {
+            cur = head;  
+        }
+        
+        return dfs(head, cur, root.left) || dfs(head, cur, root.right);
+    }
+}
